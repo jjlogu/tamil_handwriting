5f2edbb220653250f66312adc80581be44d87f77
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-01-15 15:41:33 -0600

    introducing canvas

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+<body>
+<div id="canvasDiv"></div>
+<script type="text/javascript">
+var canvasDiv = document.getElementById('canvasDiv');
+var canvasWidth  = 100;
+var canvasHeight = 100;
+
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', canvasWidth);
+canvas.setAttribute('height', canvasHeight);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+context = canvas.getContext("2d");
+</script>
+
+</body>
+</html>
