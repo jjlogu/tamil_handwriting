26466d024b24e3c7d30f71a968f91bbf38951ce5
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-01-16 19:05:51 -0600

    introducted tamil html unicode characters

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <html>
 <head>
+  <meta name="viewport" content="width=300, initial-scale=1">
   <style>
     #canvas {
       border: 1px solid gray;
     }
+    body, html {
+      height: 100%;
+      display: grid;
+    }
     #canvasDiv {
+        /* center align */
+        text-align: center;
+        margin: auto;
+
         /* Prevent nearby text being highlighted when accidentally dragging mouse outside confines of the canvas */
         -webkit-touch-callout: none;
         -webkit-user-select: none;
@@ -20,18 +29,179 @@
     crossorigin="anonymous"></script>
 </head>
 <body>
-  <div id="canvasDiv"></div>
-  <button onclick="clearCanvas()">Clear</button>
+  <?php 
+    $tamil_html_unicodes = array("&#x0B85",
+                                "&#x0B86",
+                                "&#x0B87",
+                                "&#x0B88",
+                                "&#x0B89",
+                                "&#x0B8A",
+                                "&#x0B8E",
+                                "&#x0B8F",
+                                "&#x0B90",
+                                "&#x0B92",
+                                "&#x0B93",
+                                "&#x0B83",
+                                "&#x0B95",
+                                "&#x0B99",
+                                "&#x0B9A",
+                                "&#x0B9E",
+                                "&#x0B9F",
+                                "&#x0BA3",
+                                "&#x0BA4",
+                                "&#x0BA8",
+                                "&#x0BAA",
+                                "&#x0BAE",
+                                "&#x0BAF",
+                                "&#x0BB0",
+                                "&#x0BB2",
+                                "&#x0BB5",
+                                "&#x0BB4",
+                                "&#x0BB3",
+                                "&#x0BB1",
+                                "&#x0BA9",
+                                "&#x0BB8",
+                                "&#x0BB7",
+                                "&#x0B9C",
+                                "&#x0BB9",
+                                "&#x0B95&#x0BCD&#x0BB7",
+                                "&#x0B95&#x0BBF",
+                                "&#x0B99&#x0BBF",
+                                "&#x0B9A&#x0BBF",
+                                "&#x0B9E&#x0BBF",
+                                "&#x0B9F&#x0BBF",
+                                "&#x0BA3&#x0BBF",
+                                "&#x0BA4&#x0BBF",
+                                "&#x0BA8&#x0BBF",
+                                "&#x0BAA&#x0BBF",
+                                "&#x0BAE&#x0BBF",
+                                "&#x0BAF&#x0BBF",
+                                "&#x0BB0&#x0BBF",
+                                "&#x0BB2&#x0BBF",
+                                "&#x0BB5&#x0BBF",
+                                "&#x0BB4&#x0BBF",
+                                "&#x0BB3&#x0BBF",
+                                "&#x0BB1&#x0BBF",
+                                "&#x0BA9&#x0BBF",
+                                "&#x0BB8&#x0BBF",
+                                "&#x0BB7&#x0BBF",
+                                "&#x0B9C&#x0BBF",
+                                "&#x0BB9&#x0BBF",
+                                "&#x0B95&#x0BCD&#x0BB7&#x0BBF",
+                                "&#x0B95&#x0BC0",
+                                "&#x0B99&#x0BC0",
+                                "&#x0B9A&#x0BC0",
+                                "&#x0B9E&#x0BC0",
+                                "&#x0B9F&#x0BC0",
+                                "&#x0BA3&#x0BC0",
+                                "&#x0BA4&#x0BC0",
+                                "&#x0BA8&#x0BC0",
+                                "&#x0BAA&#x0BC0",
+                                "&#x0BAE&#x0BC0",
+                                "&#x0BAF&#x0BC0",
+                                "&#x0BB0&#x0BC0",
+                                "&#x0BB2&#x0BC0",
+                                "&#x0BB5&#x0BC0",
+                                "&#x0BB4&#x0BC0",
+                                "&#x0BB3&#x0BC0",
+                                "&#x0BB1&#x0BC0",
+                                "&#x0BA9&#x0BC0",
+                                "&#x0BB8&#x0BC0",
+                                "&#x0BB7&#x0BC0",
+                                "&#x0B9C&#x0BC0",
+                                "&#x0BB9&#x0BC0",
+                                "&#x0B95&#x0BCD&#x0BB7&#x0BC0",
+                                "&#x0B95&#x0BC1",
+                                "&#x0B99&#x0BC1",
+                                "&#x0B9A&#x0BC1",
+                                "&#x0B9E&#x0BC1",
+                                "&#x0B9F&#x0BC1",
+                                "&#x0BA3&#x0BC1",
+                                "&#x0BA4&#x0BC1",
+                                "&#x0BA8&#x0BC1",
+                                "&#x0BAA&#x0BC1",
+                                "&#x0BAE&#x0BC1",
+                                "&#x0BAF&#x0BC1",
+                                "&#x0BB0&#x0BC1",
+                                "&#x0BB2&#x0BC1",
+                                "&#x0BB5&#x0BC1",
+                                "&#x0BB4&#x0BC1",
+                                "&#x0BB3&#x0BC1",
+                                "&#x0BB1&#x0BC1",
+                                "&#x0BA9&#x0BC1",
+                                "&#x0B95&#x0BC2",
+                                "&#x0B99&#x0BC2",
+                                "&#x0B9A&#x0BC2",
+                                "&#x0B9E&#x0BC2",
+                                "&#x0B9F&#x0BC2",
+                                "&#x0BA3&#x0BC2",
+                                "&#x0BA4&#x0BC2",
+                                "&#x0BA8&#x0BC2",
+                                "&#x0BAA&#x0BC2",
+                                "&#x0BAE&#x0BC2",
+                                "&#x0BAF&#x0BC2",
+                                "&#x0BB0&#x0BC2",
+                                "&#x0BB2&#x0BC2",
+                                "&#x0BB5&#x0BC2",
+                                "&#x0BB4&#x0BC2",
+                                "&#x0BB3&#x0BC2",
+                                "&#x0BB1&#x0BC2",
+                                "&#x0BA9&#x0BC2",
+                                "&#x0BBE",
+                                "&#x0BC6",
+                                "&#x0BC7",
+                                "&#x0BC8",
+                                "&#x0BB8&#x0BCD&#x0BB0&#x0BC0",
+                                "&#x0BB8&#x0BC1",
+                                "&#x0BB7&#x0BC1",
+                                "&#x0B9C&#x0BC1",
+                                "&#x0BB9&#x0BC1",
+                                "&#x0B95&#x0BCD&#x0BB7&#x0BC1",
+                                "&#x0BB8&#x0BC2",
+                                "&#x0BB7&#x0BC2",
+                                "&#x0B9C&#x0BC2",
+                                "&#x0BB9&#x0BC2",
+                                "&#x0B95&#x0BCD&#x0BB7&#x0BC2",
+                                "&#x0B95&#x0BCD",
+                                "&#x0B99&#x0BCD",
+                                "&#x0B9A&#x0BCD",
+                                "&#x0B9E&#x0BCD",
+                                "&#x0B9F&#x0BCD",
+                                "&#x0BA3&#x0BCD",
+                                "&#x0BA4&#x0BCD",
+                                "&#x0BA8&#x0BCD",
+                                "&#x0BAA&#x0BCD",
+                                "&#x0BAE&#x0BCD",
+                                "&#x0BAF&#x0BCD",
+                                "&#x0BB0&#x0BCD",
+                                "&#x0BB2&#x0BCD",
+                                "&#x0BB5&#x0BCD",
+                                "&#x0BB4&#x0BCD",
+                                "&#x0BB3&#x0BCD",
+                                "&#x0BB1&#x0BCD",
+                                "&#x0BA9&#x0BCD",
+                                "&#x0BB8&#x0BCD",
+                                "&#x0BB7&#x0BCD",
+                                "&#x0B9C&#x0BCD",
+                                "&#x0BB9&#x0BCD",
+                                "&#x0B95&#x0BCD&#x0BB7&#x0BCD",
+                                "&#x0B94");
+    foreach ($tamil_html_unicodes as $key => $tamil_html_unicode) {
+        echo $key." = ".$tamil_html_unicode."<br/>";
+    }
+  ?>
+  <div id="canvasDiv"><br/><br/><button onclick="clearCanvas()">Clear</button></div>
+  
   <script type="text/javascript">
     var canvasDiv = document.getElementById('canvasDiv');
-    var canvasWidth  = 60;
-    var canvasHeight = 60;
+    var canvasWidth  = 100;
+    var canvasHeight = 100;
 
     canvas = document.createElement('canvas');
     canvas.setAttribute('width', canvasWidth);
     canvas.setAttribute('height', canvasHeight);
     canvas.setAttribute('id', 'canvas');
-    canvasDiv.appendChild(canvas);
+    canvasDiv.prepend(canvas);
     if(typeof G_vmlCanvasManager != 'undefined') {
     	canvas = G_vmlCanvasManager.initElement(canvas);
     }
